e2c37b2e54dac0fedba6121f02c5c1fc6b06c698
Rizal Fahmi <59511280+rzalfahmy@users.noreply.github.com>
2023-07-04 22:24:32 +0700

    Add files via upload

---
+++ js/tes.js
@@ -0,0 +1,7 @@
+var pind = confirm("Ingin mengunjungi wedingin?")
+if (pind) {
+	window.location = "https://wedingin.com/";
+} else {
+	window.location = "index.html";
+	javascript:alert("Permintaan dibatalkan");
+}
