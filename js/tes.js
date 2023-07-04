7f079c7e6faf49169ba61071da52899da4cdcb4b
Rizal Fahmi <59511280+rzalfahmy@users.noreply.github.com>
2023-07-04 22:26:24 +0700

    Update tes.js

--- js/tes.js
+++ js/tes.js
@@ -2,6 +2,6 @@ var pind = confirm("Ingin mengunjungi wedingin?")
 if (pind) {
 	window.location = "https://wedingin.com/";
 } else {
-	window.location = "index.html";
+	window.location = "https://rzalfahmy.github.io/";
 	javascript:alert("Permintaan dibatalkan");
 }
